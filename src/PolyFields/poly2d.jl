913ba826c73a11a85d13ee54e8e3b5a9fcc5bfbf
Ignacio Ojea <iojea@dm.uba.ar>
2025-10-02 15:36:57 -0300

    define BivariatePolynomial

--- src/PolyFields/poly2d.jl
+++ src/PolyFields/poly2d.jl
@@ -1,5 +1,7 @@
 abstract type AbstractBivariatePolynomial <: AbstractPolynomial end
 
-struct BivariatePolynomial{F,X,Y}
-    p
+struct BivariatePolynomial{F,N,X,M,Y} <: AbstractPolynomial
+    p::ImmutablePolynomial{ImmutablePolynomial{F,X,N},Y,M}
 end
+
+_zerofill(t::NTuple{K,T},N) where {K,T} = K>=N ? t : (t...,zeros(T,N-K)...) 
