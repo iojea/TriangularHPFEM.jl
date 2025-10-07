12dea81669286a6ebca1fe0a3e405835e9ae8e50
Ignacio Ojea <iojea@dm.uba.ar>
2025-10-07 20:55:01 -0300

    CoeffType as a treat

--- src/Spaces/hpspaces.jl
+++ src/Spaces/hpspaces.jl
@@ -32,8 +32,7 @@ end
 
 LinearAlgebra.:⋅(a::VectorHPSpace,b::VectorHPSpace) = OperationField(⋅,(a,b))
 Base.:*(a::ScalarHPSpace,b::AbstractHPSpace) = OperationField(*,(a,b))
-Base.:*(m::Array,a::VectorHPSpace) = OperationField(*,(m,a))
-Base.:*(f::Function,a::VectorHPSpace) = OperationField(*,(f,a))
+Base.:*(m::Array,a::VectorHPSpace) = OperationField(*,(m,a))Base.:*(f::Function,a::VectorHPSpace) = OperationField(*,(f,a))
 
 
 
@@ -44,6 +43,13 @@ abstract type CoeffType end
 
 struct Constant <: CoeffType end
 struct Variable <: CoeffType end
+
+coefftype(::AbstractHPSpace) = Constant()
+coefftype(::AbstractArray) = Constant()
+coefftype(::Number) = Constant()
+
+coefftype(::Function) = Variable()
+coefftype(p::PolyField) = degree(p)==0 ? Constant() : Variable()
 
 # Sintactic sugar
 struct Integrand
